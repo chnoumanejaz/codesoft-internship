9321fa0ad8281a6a118091f1fb740ff4e7a26fe6
M. Nouman Ejaz <noumanejaz92@gmail.com>
2023-08-12 16:55:37 +0500

    Porrtfolio  task 90% completed

--- Task2 - Portfolio/assets/js/main.js
+++ Task2 - Portfolio/assets/js/main.js
@@ -30,15 +30,96 @@ tabs.forEach(tab => {
 
 //  -------------- MIXITUP FILTER PORTFOLIO -------------
 
+let mixerPortfolio = mixitup('.work__container', {
+  selectors: {
+    target: '.work__card',
+  },
+  animation: {
+    duration: 300,
+  },
+});
+
 //  ------ Link Active Work
+const linkWork = document.querySelectorAll('.work__item');
+
+function activeWork() {
+  linkWork.forEach(work => {
+    work.classList.remove('active-work');
+    this.classList.add('active-work');
+  });
+}
+
+linkWork.forEach(work => work.addEventListener('click', activeWork));
 
 //  ------ Work Popup
+document.addEventListener('click', function (e) {
+  if (e.target.classList.contains('work__button')) {
+    togglePortfolioPopup();
+    portfolioItemDetails(e.target.parentElement);
+  }
+});
+
+function togglePortfolioPopup() {
+  document.querySelector('.portfolio__popup').classList.toggle('open');
+}
+
+document
+  .querySelector('.portfolio__popup-close')
+  .addEventListener('click', togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem) {
+  document.querySelector('.pp__thumbnail img').src =
+    portfolioItem.querySelector('.work__img').src;
+  document.querySelector('.pp__thumbnail img').alt =
+    portfolioItem.querySelector('.work__img').alt;
+
+  document.querySelector('.portfolio__popup-subtitle span').innerHTML =
+    portfolioItem.querySelector('.work__title').innerHTML;
+  document.querySelector('.portfolio__popup-body').innerHTML =
+    portfolioItem.querySelector('.portfolio__item-details').innerHTML;
+}
 
 //  SERVICES MODAL
+const modelViews = document.querySelectorAll('.services__model'),
+  modelBtns = document.querySelectorAll('.services__button'),
+  modelClose = document.querySelectorAll('.services__model-close');
 
-//  SWIPER TESTIMONIAL
+let model = function (modelClick) {
+  modelViews[modelClick].classList.add('active-model');
+};
+
+modelBtns.forEach((modelBtn, i) => {
+  modelBtn.addEventListener('click', function () {
+    model(i);
+  });
+});
+
+modelClose.forEach(close => {
+  close.addEventListener('click', () => {
+    modelViews.forEach(modelView => {
+      modelView.classList.remove('active-model');
+    });
+  });
+});
 
 //  INPUT ANIMATION
+const inputs = document.querySelectorAll('.input');
+function focus() {
+  let parent = this.parentNode;
+  parent.classList.add('focus');
+}
+
+function blur() {
+  let parent = this.parentNode;
+  if (this.value.trim() === '') {
+    parent.classList.remove('focus');
+  }
+}
+
+inputs.forEach(input => {
+  input.addEventListener('focus', focus);
+  input.addEventListener('blur', blur);
+});
 
 //  SCROLL SECTIONS ACTIVE LINK
 
